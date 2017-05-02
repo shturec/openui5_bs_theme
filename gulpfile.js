5d0beeac84575d22aa213a94367ff90468cf715e
shturec <sthrakal@gmail.com>
2017-05-03 00:43:34 +0300

    travis ci fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var csso = require('gulp-csso');
+var rename = require("gulp-rename");
 
-gulp.task('default', ['move_css']);
-
-gulp.task('move_css', function(){
-	gulp.watch("./*.css", function(){
-		gulp.src("./*.css").pipe(gulp.dest("."));
-		console.log("Minified and Moved CSS");
-	});
+gulp.task('concat_css', function(){
+	return gulp.src(['./common/**/*.css','./shell/**/*.css', './buttons/**/*.css','./navbar/**/*.css','./tables/**/*.css'])
+			.pipe(concatCss("theme.css"))
+			.pipe(gulp.dest('./dist'))
 });
+
+gulp.task('minify_css', function () {
+    return gulp.src('./dist/theme.css')
+			.pipe(csso())
+			.pipe(rename("theme.min.css"))
+			.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['concat_css', 'minify_css']);

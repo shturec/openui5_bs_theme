fa60f0b62c4046a9669b954d00889a3a695328fe
shturec <sthrakal@gmail.com>
2017-05-02 23:44:38 +0300

    removed csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp');
-var csso = require('gulp-csso');
 var concat = require('gulp-concat');
 
 gulp.task('default', ['move_css']);
 
 gulp.task('move_css', function(){
 	gulp.watch("./*.css", function(){
-		gulp.src("./*.css").pipe(csso()).pipe(gulp.dest("."));
+		gulp.src("./*.css").pipe(gulp.dest("."));
 		console.log("Minified and Moved CSS");
 	});
 });
